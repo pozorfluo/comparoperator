39e5a86d974fd28184f68f328be2cb0f63ececcd
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-06-12 22:10:15 +0200

    Add database setup .sql

--- README.md
+++ README.md
@@ -4,45 +4,45 @@
 
 1. Clone this repository
 
-   ```shell
-   git clone --depth=1 https://github.com/pozorfluo/comparoperator.git
-   ```
+    ```shell
+    git clone --depth=1 https://github.com/pozorfluo/comparoperator.git
+    ```
 
 1. Setup
 
-   ```shell
-   cd scripts
-   ./setup.sh
-   ```
+    ```shell
+    cd scripts
+    ./setup.sh
+    ```
 
 1. Setup the database with :
 
-   ```
-   resources/sql/bootstrap.sql
-   ```
+    ```
+    resources/sql/comparoperator.sql
+    ```
 
-   _( note : this will bootstrap the database with test data )_
+    ~~_( note : this will bootstrap the database with test data )_~~
 
 1. In index.php, change DEV_FORCE_CONFIG_UPDATE to true
-   ```php
-   define('DEV_FORCE_CONFIG_UPDATE', true);
-   ```
+    ```php
+    define('DEV_FORCE_CONFIG_UPDATE', true);
+    ```
 1. Navigate to index.php in your environment
-   - A .env file is created with a skeleton config for this app.
-   - Update the default db configuration in .env to match your environment.
-     e.g.,
-   ```json
-     "db_configs": {
-       "product_hunt": {
-       "DB_DRIVER": "mysql",
-       "DB_HOST": "127.0.0.1",
-       "DB_PORT": "3306",
-       "DB_CHARSET": "utf8mb4",
-       "DB_NAME": "tp_product_hunt",
-       "DB_USER": "your_user_name",
-       "DB_PASSWORD": "your_db_password"
-       }
-   ```
+    - A .env file is created with a skeleton config for this app.
+    - Update the default db configuration in .env to match your environment.
+      e.g.,
+    ```json
+      "db_configs": {
+        "product_hunt": {
+        "DB_DRIVER": "mysql",
+        "DB_HOST": "127.0.0.1",
+        "DB_PORT": "3306",
+        "DB_CHARSET": "utf8mb4",
+        "DB_NAME": "tp_product_hunt",
+        "DB_USER": "your_user_name",
+        "DB_PASSWORD": "your_db_password"
+        }
+    ```
 1. Navigate to index.php
 
 ## build
@@ -50,7 +50,7 @@
 1. Install dependencies
 
     ```shell
-    cd 
+    cd
     npm i
     ```
 
@@ -100,8 +100,7 @@ see [project board]()
 
 ## decisions log
 
-- Follow PSR-1 and PSR-12 coding standards.
-- Use github project board.
+-   Follow PSR-1 and PSR-12 coding standards.
+-   Use github project board.
 
 ## reference links
-
---
+++ resources/sql/comparoperator.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: May 28, 2019 at 11:47 AM
+-- Server version: 10.3.4-MariaDB
+-- PHP Version: 7.2.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ComparOperator`
+--
+CREATE DATABASE IF NOT EXISTS `ComparOperator` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ComparOperator`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `id` int(10) NOT NULL,
+  `location` varchar(150) NOT NULL,
+  `price` int(10) NOT NULL,
+  `id_tour_operator` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `destinations`
+--
+
+INSERT INTO `destinations` (`id`, `location`, `price`, `id_tour_operator`) VALUES
+(1, 'corse', 500, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(10) NOT NULL,
+  `message` varchar(250) NOT NULL,
+  `author` varchar(150) NOT NULL,
+  `id_tour_operator` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `message`, `author`, `id_tour_operator`) VALUES
+(1, 'super club !', 'alex', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tour_operators`
+--
+
+CREATE TABLE `tour_operators` (
+  `id` int(10) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `grade` int(2) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `is_premium` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tour_operators`
+--
+
+INSERT INTO `tour_operators` (`id`, `name`, `grade`, `link`, `is_premium`) VALUES
+(1, 'club med', 5, 'https://www.clubmed.fr/', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_operator` (`id_tour_operator`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_tour_operator` (`id_tour_operator`);
+
+--
+-- Indexes for table `tour_operators`
+--
+ALTER TABLE `tour_operators`
+  ADD PRIMARY KEY (`id`);
+
+
+--
+-- AUTO_INCREMENT for table `destinations`
+--
+ALTER TABLE `destinations`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tour_operators`
+--
+ALTER TABLE `tour_operators`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+
+--
+-- Constraints for table `destinations`
+--
+ALTER TABLE `destinations`
+  ADD CONSTRAINT `fk_operator` FOREIGN KEY (`id_tour_operator`) REFERENCES `tour_operators` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `fk_tour_operator` FOREIGN KEY (`id_tour_operator`) REFERENCES `tour_operators` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
