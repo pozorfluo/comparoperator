9181030f7979b7ae57a46f002b056f59c31b2140
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-06-13 14:14:17 +0200

    Move php vendor binaries to scripts

--- scripts/phpunit_playground.sh
+++ scripts/phpunit_playground.sh
@@ -7,3 +7,8 @@ php composer.phar dump-autoload
 # run tests
 # ./vendor/bin/phpunit tests --bootstrap src/Helpers/AutoLoader.php
 ./vendor/bin/phpunit tests
+
+# install along with phpspec
+php composer.phar remove phpunit/phpunit --dev
+php composer.phar require --dev phpspec/phpspec
+php composer.phar require --dev phpunit/phpunit
